45f8d43b96d4ba7e2dd6ad2bf3c8bc7d64413cf4
unknown <mathieu.magnin@gmail.com>
2012-11-22 14:59:53 +0100

    binding sur le model

--- intro/etape1.js
+++ intro/etape1.js
@@ -22,3 +22,44 @@ var superView = new MaView({model: superModel});
 superModel.set('age', 15);
 
 //////////////////////////////////////////////////////////////////
+
+
+var MaView = Backbone.View.extend({
+
+	initialize: function(){
+		this.model.on('change', this.render, this);
+	},
+
+	render: function(){
+		$('#ici').html('Age: ' + this.model.get('age')); // Bahh cracra
+	}
+});
+
+var superView = new MaView({model: superModel});
+
+superView.render();
+
+//////////////////////////////////////////////////////////////////
+
+superModel.set('age', 36);
+
+//////////////////////////////////////////////////////////////////
+
+var MaView = Backbone.View.extend({
+
+	initialize: function(){
+		this.model.on('change', this.render, this);
+		$('input').change(function(){
+			that.model.set('age', $('input').val());
+		});
+	},
+
+	render: function(){
+		$('#ici').html('Age: ' + this.model.get('age')); // Bahh cracra
+		var that = this;
+	}
+});
+
+var superView = new MaView({model: superModel});
+
+superView.render();
